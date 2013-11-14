c25d58ad14950bc4596502b15c4826f9af959759
leniza19 <leniza.enikeeva@gmail.com>
2013-11-15 01:07:50 +0500

    Создала общий класс для отображения информации о сотрудниках EmployeeServlet.java.

--- mathchem/web/employee/EmployeeServlet.java
+++ mathchem/web/employee/EmployeeServlet.java
@@ -14,21 +14,30 @@ import javax.servlet.http.HttpServletResponse;
 import mathchem.data.ExpItem;
 import mathchem.data.PeroksidDAO;
 
-public class IrekmarsServlet extends HttpServlet
+public class EmployeeServlet extends HttpServlet
 {
 	private RequestDispatcher jsp;
 
 	public void init(ServletConfig config) throws ServletException {
 		ServletContext context = config.getServletContext();
-		jsp = context.getRequestDispatcher("/WEB-INF/jsp/employee/irekmars.jsp");
+		String EmpName = config.getInitParameter("employeeName");			
+		
+		//System.out.println(EmpName);
+		
+		if (EmpName.compareTo("irekmars") == 0) {
+			jsp = context.getRequestDispatcher("/WEB-INF/jsp/employee/irekmars.jsp");
+		} else if (EmpName.compareTo("spivak") == 0) {
+			jsp = context.getRequestDispatcher("/WEB-INF/jsp/employee/irekmars.jsp");
+		}
+		
 	}
 
 
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {		
 
-		List<ExpItem> zamerItems = new PeroksidDAO().getExpData();
-		req.setAttribute("zamerItems", zamerItems);
+		//List<ExpItem> zamerItems = new PeroksidDAO().getExpData();
+		//req.setAttribute("zamerItems", zamerItems);
 		jsp.forward(req, resp);
 	}
 }
